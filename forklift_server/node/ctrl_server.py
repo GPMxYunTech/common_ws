a1b33d13df137f4871fa263b6eb927d3e640d4c9
M11112089 <M11112089@yuntech.edu.tw>
2023-02-01 12:41:29 +0800

    _

---
+++ forklift_server/node/ctrl_server.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import rospy
+import actionlib
+import forklift_server.msg
+
+def PBVS_client(msg):
+    client = actionlib.SimpleActionClient('PBVS', forklift_server.msg.PBVSAction)
+    client.wait_for_server()
+    command = forklift_server.msg.PBVSGoal(command=msg)
+    print("send ", command)
+    client.send_goal(command)
+    client.wait_for_result()
+    return client.get_result()
+
+if __name__ == '__main__':
+    rospy.init_node('ctrl_server')
+    command = ["parking", "up", "down"]
+    for msg in command:
+        print("send ", msg)
+        result = PBVS_client(msg)
+        print("result ", result)
+  
+    
