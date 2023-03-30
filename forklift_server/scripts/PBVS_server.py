871508b3e3373b9fbdd11ebfde560a4b5a8aab9e
M11112089 <M11112089@yuntech.edu.tw>
2023-03-30 13:58:44 +0800

    將視窗視為可選項，將TrustworthyMarker2DTheta移動到PBVS_Action中

--- forklift_server/scripts/PBVS_server.py
+++ forklift_server/scripts/PBVS_server.py
@@ -10,7 +10,7 @@ from nav_msgs.msg import Odometry
 import math
 from gpm_msg.msg import forkposition
 from ekf import KalmanFilter
-import statistics
+import tkinter as tk
 
 class Subscriber():
     def __init__(self):
@@ -24,6 +24,9 @@ class Subscriber():
         self.sub_forwardbackpostion = rospy.Subscriber(forkpos, forkposition, self.cbGetforkpos, queue_size = 1)
         self.ekf_theta = KalmanFilter()
         self.init_parame()
+
+        if(rospy.get_param(rospy.get_name() + "/gui", True)):
+            self.windows()
 
     def init_parame(self):
         # Odometry_param
@@ -43,6 +46,8 @@ class Subscriber():
         self.updownposition = 0.0
         #ekf
         self.ekf_theta.init(1,1,5)
+    def __del__(self):
+        self.window.destroy()
 
     def SpinOnce(self):
         return self.robot_2d_pose_x, self.robot_2d_pose_y, self.robot_2d_theta, \
@@ -61,9 +66,6 @@ class Subscriber():
                 self.marker_2d_pose_x = -marker_msg.position.z
                 self.marker_2d_pose_y = marker_msg.position.x
                 self.marker_2d_theta = -theta
-                print("marker_2d_theta", math.degrees(self.marker_2d_theta))
-                print("self.marker_2d_pose_x", self.marker_2d_pose_x)
-                print("self.marker_2d_pose_y", self.marker_2d_pose_y)
             else:
                 pass
         except:
@@ -117,28 +119,83 @@ class Subscriber():
         self.forwardbackpostion = msg.forwardbackpostion
         self.updownposition = msg.updownposition
 
-    def TrustworthyMarker2DTheta(self, time):
-        rospy.sleep(0.5)
-        marker_2d_theta_list = [0.0]
-        initial_time = rospy.Time.now().secs
-        print("self.marker_2d_theta_1", self.marker_2d_theta)
-        while(abs(initial_time - rospy.Time.now().secs) < time):
-            marker_2d_theta_list.append(self.marker_2d_theta)
-            print("self.marker_2d_theta", self.marker_2d_theta)
-            rospy.sleep(0.05)
-        print("marker_2d_theta_list", marker_2d_theta_list)
-        threshold = 0.5
-        mean = statistics.mean(marker_2d_theta_list)
-        stdev = statistics.stdev(marker_2d_theta_list)
-        upcutoff = mean + threshold * stdev
-        downcutoff = mean - threshold * stdev
-        clean_list = []
-        for i in marker_2d_theta_list:
-            if(i > downcutoff and i < upcutoff):
-               clean_list.append(i)
-               
-        return statistics.mean(clean_list)      
-        # print(statistics.mean(clean_list))  
+    def windows(self):
+
+        self.window = tk.Tk()
+        self.window.geometry('220x170+1700+560') 
+
+        self.labelrobot_2d_pose_x = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')
+        self.label_robot_2d_pose_x = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black') 
+
+        self.labelrobot_2d_pose_y= tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')
+        self.label_robot_2d_pose_y = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')  
+
+        self.labelrobot_2d_theta= tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')
+        self.label_robot_2d_theta = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')   
+
+        self.labelmarker_2d_pose_x= tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')
+        self.label_marker_2d_pose_x = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')        
+
+        self.labelmarker_2d_pose_y= tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')
+        self.label_marker_2d_pose_y = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')     
+
+        self.labelmarker_2d_theta= tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')
+        self.label_marker_2d_theta = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black') 
+
+        self.labelfork_pose_updownposition= tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')
+        self.label_fork_pose_updownposition = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black') 
+        
+        self.labelfork_pose_forwardbackpostion= tk.Label(self.window, text="", font=('Helvetica', 10), fg='black')
+        self.label_fork_pose_forwardbackpostion = tk.Label(self.window, text="", font=('Helvetica', 10), fg='black') 
+        self.update_window()
+        self.window.mainloop()
+
+    def update_window(self):
+        base = 0
+        
+        base1 = base
+        self.labelrobot_2d_pose_x.configure(text='Robot 2d Pose x: ')
+        self.labelrobot_2d_pose_x.place(x=0, y=base1)        
+        self.label_robot_2d_pose_x.configure(text=self.robot_2d_pose_x)
+        self.label_robot_2d_pose_x.place(x=190, y=base1)
+
+        self.labelrobot_2d_pose_y.configure(text="Robot 2d Pose y: ")
+        self.labelrobot_2d_pose_y.place(x=0, y=base1+20)
+        self.label_robot_2d_pose_y.place(x=190, y=base1+20)
+        self.label_robot_2d_pose_y.configure(text=self.robot_2d_pose_y)
+
+        self.labelrobot_2d_theta.configure(text="Robot 2d theta: ")
+        self.labelrobot_2d_theta.place(x=0, y=base1+40)
+        self.label_robot_2d_theta.place(x=190, y=base1+40)
+        self.label_robot_2d_theta.configure(text=math.degrees(self.robot_2d_theta))
+
+        base2 = base1+60
+        self.labelmarker_2d_pose_x.configure(text="Marker 2d Pose x: ")
+        self.labelmarker_2d_pose_x.place(x=0, y=base2)
+        self.label_marker_2d_pose_x.place(x=190, y=base2)
+        self.label_marker_2d_pose_x.configure(text=self.marker_2d_pose_x)
+
+        self.labelmarker_2d_pose_y.configure(text="Marker 2d Pose y: ")
+        self.labelmarker_2d_pose_y.place(x=0, y=base2+20)
+        self.label_marker_2d_pose_y.place(x=190, y=base2+20)
+        self.label_marker_2d_pose_y.configure(text=self.marker_2d_pose_y)
+
+        self.labelmarker_2d_theta.configure(text="Marker 2d theta: ")
+        self.labelmarker_2d_theta.place(x=0, y=base2+40)
+        self.label_marker_2d_theta.place(x=190, y=base2+40)
+        self.label_marker_2d_theta.configure(text=self.marker_2d_theta)
+
+        self.labelfork_pose_updownposition.configure(text="Fork updown position: ")
+        self.labelfork_pose_updownposition.place(x=0, y=base2+60)
+        self.label_fork_pose_updownposition.place(x=190, y=base2+60)
+        self.label_fork_pose_updownposition.configure(text=self.updownposition)
+
+        self.labelfork_pose_forwardbackpostion.configure(text="Fork forwardback position: ")
+        self.labelfork_pose_forwardbackpostion.place(x=0, y=base2+80)
+        self.label_fork_pose_forwardbackpostion.place(x=190, y=base2+80)
+        self.label_fork_pose_forwardbackpostion.configure(text=self.forwardbackpostion)
+
+        self.window.after(50, self.update_window)
 
 class PBVSAction():
     def __init__(self, name):
